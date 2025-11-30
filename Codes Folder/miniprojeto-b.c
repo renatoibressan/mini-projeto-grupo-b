6ebbb0a2980bf950eedbf1fa5e6ea0d969e25f30
Renato Ikeda Bressan <renatoikeda98@gmail.com>
2025-11-30 12:32:51 -0300

    Declaração de structs e funções

--- Codes Folder/miniprojeto-b.c
+++ Codes Folder/miniprojeto-b.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Data {
+  int dia, mes, ano;
+} data;
+
+typedef struct Pessoa {
+  char nome[100];
+  char cpf[20];
+  data data_nascimento;
+  char telefone[20];
+} pessoa;
+
+typedef struct Terrenos {
+  int id;
+  pessoa dono;
+  data data_compra;
+  float largura, comprimento, area, preco_m2;
+} terreno;
+
+void inicializarVetor(terreno **terrenos);
+
+void criarTerreno(terreno **terrenos);
+
+void deletarTerreno(terreno **terrenos);
+
+void mostrarTerreno(terreno **terrenos, int id);
+
+void editarTerreno(terreno **terrenos, int id);
+
+double calcularValorTerreno(terreno **terrenos, int id);
+
+int contarTerrenosOcupados(terreno **terrenos);
+
+int contarTerrenosLivres(terreno **terrenos);
+
+double calcularValorTotal(terreno **terrenos);
+
+void salvarTerrenos(terreno **terrenos, const char *nomeArquivo);
+
+void carregarTerrenos(terreno **terrenos, const char *nomeArquivo);
+
 int main() {
   return 0;
 }
