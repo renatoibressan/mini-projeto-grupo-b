b9d65611edbee5235add8ac4b85b187371176218
Renato Ikeda Bressan <renatoikeda98@gmail.com>
2025-12-01 16:47:15 -0300

    Comentários para determinar quem implementará cada função

--- Codes Folder/miniprojeto-b.c
+++ Codes Folder/miniprojeto-b.c
@@ -19,27 +19,27 @@ typedef struct Terrenos {
   float largura, comprimento, area, preco_m2;
 } terreno;
 
-void inicializarVetor(terreno **terrenos);
+void inicializarVetor(terreno **terrenos); // Function 1 - Aryan
 
-void criarTerreno(terreno **terrenos);
+void criarTerreno(terreno **terrenos); // Function 2 - Aryan
 
-void deletarTerreno(terreno **terrenos);
+void deletarTerreno(terreno **terrenos); // Function 3 - Amanda
 
-void mostrarTerreno(terreno **terrenos, int id);
+void mostrarTerreno(terreno **terrenos, int id); // Function 4 - Aryan
 
-void editarTerreno(terreno **terrenos, int id);
+void editarTerreno(terreno **terrenos, int id); // Function 5 - Renato
 
-double calcularValorTerreno(terreno **terrenos, int id);
+double calcularValorTerreno(terreno **terrenos, int id); // Function 6 - Aryan
 
-int contarTerrenosOcupados(terreno **terrenos);
+int contarTerrenosOcupados(terreno **terrenos); // Function 7 - Renato
 
-int contarTerrenosLivres(terreno **terrenos);
+int contarTerrenosLivres(terreno **terrenos); // Function 8 - Amanda
 
-double calcularValorTotal(terreno **terrenos);
+double calcularValorTotal(terreno **terrenos); // Function 9 - Amanda
 
-void salvarTerrenos(terreno **terrenos, const char *nomeArquivo);
+void salvarTerrenos(terreno **terrenos, const char *nomeArquivo); // Function 10 - Amanda
 
-void carregarTerrenos(terreno **terrenos, const char *nomeArquivo);
+void carregarTerrenos(terreno **terrenos, const char *nomeArquivo); // Function 11 - Renato
 
 int main() {
   return 0;
