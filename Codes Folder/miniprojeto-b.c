74d2cab481e2ad81ebdf4e73383fa7bab12cc194
Renato Ikeda Bressan <renatoikeda98@gmail.com>
2025-12-02 13:49:57 -0300

    Ajuste de formatação

--- Codes Folder/miniprojeto-b.c
+++ Codes Folder/miniprojeto-b.c
@@ -4,14 +4,12 @@
 typedef struct Data {
   int dia, mes, ano;
 } data;
-
 typedef struct Pessoa {
   char nome[100];
   char cpf[20];
   data data_nascimento;
   char telefone[20];
 } pessoa;
-
 typedef struct Terrenos {
   int id;
   pessoa dono;
@@ -20,25 +18,15 @@ typedef struct Terrenos {
 } terreno;
 
 void inicializarVetor(terreno **terrenos); // Function 1 - Aryan
-
 void criarTerreno(terreno **terrenos); // Function 2 - Aryan
-
 void deletarTerreno(terreno **terrenos); // Function 3 - Amanda
-
 void mostrarTerreno(terreno **terrenos, int id); // Function 4 - Aryan
-
 void editarTerreno(terreno **terrenos, int id); // Function 5 - Renato
-
 double calcularValorTerreno(terreno **terrenos, int id); // Function 6 - Aryan
-
 int contarTerrenosOcupados(terreno **terrenos); // Function 7 - Renato
-
 int contarTerrenosLivres(terreno **terrenos); // Function 8 - Amanda
-
 double calcularValorTotal(terreno **terrenos); // Function 9 - Amanda
-
 void salvarTerrenos(terreno **terrenos, const char *nomeArquivo); // Function 10 - Amanda
-
 void carregarTerrenos(terreno **terrenos, const char *nomeArquivo); // Function 11 - Renato
 
 int main(const char *nomeArquivo) {
