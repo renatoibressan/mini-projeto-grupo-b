e624149502f495b26b1e1912ef48b7e519765c4d
Renato Ikeda Bressan <renatoikeda98@gmail.com>
2025-12-04 09:01:36 -0300

    Modelagem da main

--- Codes Folder/main.c
+++ Codes Folder/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "terrenos.h"
 
 int main(const char *nomeArquivo) {
+  int option = -1;
+  terreno **t;
+  FILE *arq = NULL;
+  strcpy(nomeArquivo, "arq");
+  while (option != 0) {
+    option = -1;
+    scanf("%d", &option);
+  }
   return 0;
 }
--- Codes Folder/terrenos.c
+++ Codes Folder/terrenos.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "terrenos.h"
 
 void inicializarVetor(terreno **terrenos) {
