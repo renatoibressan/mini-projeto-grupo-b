ca9cc441f5fc34fdfd5ae0f30b99056847ee12e7
Renato Ikeda Bressan <renatoikeda98@gmail.com>
2025-12-04 08:46:38 -0300

    Modificações em terrenos.c

--- Codes Folder/terrenos.c
+++ Codes Folder/terrenos.c
@@ -1,3 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "terrenos.h"
+
+void inicializarVetor(terreno **terrenos) {
+} // Function 1 - Aryan
+void criarTerreno(terreno **terrenos) {
+} // Function 2 - Aryan
+void deletarTerreno(terreno **terrenos) {
+} // Function 3 - Amanda
+void mostrarTerreno(terreno **terrenos, int id) {
+} // Function 4 - Aryan
+void editarTerreno(terreno **terrenos, int id) {
+} // Function 5 - Renato
+double calcularValorTerreno(terreno **terrenos, int id) {
+} // Function 6 - Aryan
+int contarTerrenosOcupados(terreno **terrenos) {
+} // Function 7 - Renato
+int contarTerrenosLivres(terreno **terrenos) {
+} // Function 8 - Amanda
+double calcularValorTotal(terreno **terrenos) {
+} // Function 9 - Amanda
+void salvarTerrenos(terreno **terrenos, const char *nomeArquivo) {
+} // Function 10 - Amanda
+void carregarTerrenos(terreno **terrenos, const char *nomeArquivo) {
+} // Function 11 - Renato
